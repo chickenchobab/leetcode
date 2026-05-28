51f19359695e9c3f187ed31c2b26f86faa3be8d0
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2026-05-28 20:33:57 +0900

    Time: 0 ms (100%), Space: 8.8 MB (11.14%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,9 +2,13 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
 
-        int mn = min(word1.size(), word2.size());
+        int a = word1.size();
+        int b = word2.size();
+        int mn = min(a, b);
+        int mx = max(a, b);
+
         string output;
-        output.resize(word1.size() + word2.size());
+        output.resize(a + b);
 
         for (int i = 0; i < mn; ++i)
         {
@@ -12,9 +16,9 @@ public:
             output[i * 2 + 1] = word2[i];
         }
 
-        const string& longer = (mn == word1.size() ? word2 : word1);
+        const string& longer = (a < b ? word2 : word1);
 
-        for (int i = 2 * mn, j = mn; j < longer.size(); ++i, ++j)
+        for (int i = 2 * mn, j = mn; j < mx; ++i, ++j)
         {
             output[i] = longer[j];
         }
