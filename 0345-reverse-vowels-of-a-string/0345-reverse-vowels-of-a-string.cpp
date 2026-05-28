ba4356eef1a7bd15b61097190c75924472344c4a
chickenchobab <106914762+chickenchobab@users.noreply.github.com>
2026-05-28 23:06:35 +0900

    Time: 0 ms (100%), Space: 10.3 MB (74.6%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,37 +2,28 @@ class Solution {
 public:
     bool IsVowel(char c)
     {
-        return c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U';
+        c = tolower(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
+
     string reverseVowels(string s) {
-        vector<char> vowels;
-        int l = s.length();
-        vowels.reserve(l);
-
-        for (char c : s)
+        int a = 0, b = s.length() - 1;
+        while (a < b)
         {
-            if (IsVowel(c))
+            while (a < b && !IsVowel(s[a]))
+                ++a;
+            
+            while (a < b && !IsVowel(s[b]))
+                --b;
+
+            if (a < b)
             {
-                vowels.push_back(c);
+                std::swap(s[a], s[b]);
+                ++a;
+                --b;    
             }
         }
 
-        string output;
-        output.resize(l);
-
-        int v = vowels.size() - 1;
-        for (int i = 0; i < l; ++i)
-        {
-            if (IsVowel(s[i]))
-            {
-                output[i] = vowels[v--];
-            }
-            else
-            {
-                output[i] = s[i];
-            }
-        }
-
-        return output;
+        return s;
     }
 };
